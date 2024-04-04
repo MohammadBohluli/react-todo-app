9d987897855d8f0f4ac66dfc64b3a806dac0c27e
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-04-04 12:59:09 +0330

    styling InputTask

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,5 +1,5 @@
 import { FormEvent } from "react";
-
+import { VscAdd } from "react-icons/vsc";
 interface Props {
   value: string;
   onSubmit: (e: FormEvent<HTMLFormElement>) => void;
@@ -8,13 +8,16 @@ interface Props {
 
 const InputTask = ({ value, onSubmit, onChange }: Props) => {
   return (
-    <form onSubmit={(e) => onSubmit(e)}>
+    <form onSubmit={(e) => onSubmit(e)} className="flex my-9 gap-4">
       <input
         value={value}
         type="text"
         onChange={(e) => onChange(e.target.value)}
+        className="grow rounded pr-2 py-1 outline-none border-b-4 border-white focus:border-b-sky-400"
       />
-      <button type="submit">Submit</button>
+      <button type="submit" className="bg-sky-400 p-1 rounded hover:bg-sky-500">
+        <VscAdd color="white" size={25} />
+      </button>
     </form>
   );
 };
