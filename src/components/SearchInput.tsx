9b359dec473e5817db6de1ceea10a2ce109c5058
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2024-04-06 13:23:29 +0330

    change styling input search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,3 +1,4 @@
+import { CiSearch } from 'react-icons/ci';
 interface Props {
   searchQuery: string;
   onSearchQuery: (searchParam: string) => void;
@@ -5,12 +6,18 @@ interface Props {
 
 const SearchInput = ({ searchQuery, onSearchQuery }: Props) => {
   return (
-    <input
-      type="search"
-      value={searchQuery}
-      onChange={(e) => onSearchQuery(e.target.value)}
-      className="input"
-    />
+    <div className="flex justify-center items-center relative">
+      <span className="bg-white absolute right-0 text-gray-400">
+        <CiSearch size={30} />
+      </span>
+      <input
+        type="search"
+        value={searchQuery}
+        onChange={(e) => onSearchQuery(e.target.value)}
+        className="input pr-8"
+        placeholder="جست و جو ..."
+      />
+    </div>
   );
 };
 
